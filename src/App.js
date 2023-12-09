17dce3b4ce648c579d63ea1e44b7edb78288336e
Michelle Tan <mtan22@calpoly.edu>
2023-12-09 13:43:21 -0800

    removed unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // App.js
 import React from 'react';
 import Map from './components/Map';
-import './App.css'; // Import your modified CSS file
+import './App.css'; 
 
 const App = () => {
   return (
-    <div className="App-header"> {/* Change the class name here */}
+    <div className="App-header">
       <h1>POLYPARKING</h1>
       <Map />
     </div>
